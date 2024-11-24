ba17f1572d3606d9b64f2a92f90ebcfe973e198a
Oyedelehabeeb <hoyedeley4@gmail.com>
2024-11-24 06:19:58 +0100

    update about page text

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,12 +5,12 @@ import logo from "@/public/logo.png";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Opal Horizon" /> */}
       <Image
         src={logo}
         height="60"
         width="60"
-        alt="The Wild Oasis logo"
+        alt="The Opal Horizon logo"
         quality={100}
       />
       <span className="text-xl font-semibold text-primary-100">
