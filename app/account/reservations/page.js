2683d6a26d6e10d414456b83bbb712b3f028c0fb
Oyedelehabeeb <hoyedeley4@gmail.com>
2024-12-07 17:09:06 +0100

    new update

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,7 +1,6 @@
-import ReservationCard from "@/app/_components/ReservationCard";
 import ReservationList from "@/app/_components/ReservationList";
 import { auth } from "@/app/_lib/auth";
-import { getBookings, getCabinsById } from "@/app/_lib/data-service";
+import { getBookings } from "@/app/_lib/data-service";
 import Link from "next/link";
 
 export const metadata = {
